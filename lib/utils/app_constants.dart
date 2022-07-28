b568ae85262434506aa5dde4184a58a23ae5c68a
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-07-28 19:39:28 +0600

    sfwe

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String appName = 'Food App';
+  static const int appVersion = 1;
+
+  static const String baseURL = "https://mvs.bslmeiyu.com";
+  static const String populerProductURL = "/api/v1/products/popular";
+  static const String recommendedProductURL = "/api/v1/products/recommended";
+
+  static const String token = 'BDtoken';
+}
